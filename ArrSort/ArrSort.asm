791953520fb9c3b971205c54c4fb0b9a3b0a20a1
DuyDo24 <a1921156@adelaide.edu.au>
2025-09-09 20:17:23 +0930

    f

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -2,36 +2,76 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-LOAD R3, R2
-SUB R3, R3, #1
-
-OUTER_LOOP:
-    CMP R3, #0
-    BEQ SORT_COMPLETE
-    LOAD R4, #0
-    
-    INNER_LOOP:
-        CMP R4, R3
-        BGE NEXT_PASS
-        ADD R5, R1, R4
-        LOAD R5, [R5]
-        ADD R6, R1, R4
-        ADD R6, R6, #1
-        LOAD R6, [R6]
-        CMP R5, R6
-        BLE NO_SWAP
-        ADD R7, R1, R4
-        STORE [R7], R6
-        ADD R7, R7, #1
-        STORE [R7], R5
-        
-    NO_SWAP:
-        ADD R4, R4, #1
-        B INNER_LOOP
-    
-    NEXT_PASS:
-        SUB R3, R3, #1
-        B OUTER_LOOP
-
-SORT_COMPLETE:
-    LOAD R0, #-1
+@2
+D=M
+@10
+M=D-1
+(OUTER)
+@10
+D=M
+@END
+D;JEQ
+@20
+M=0
+(INNER)
+@20
+D=M
+@10
+D=D-M
+@NEXT
+D;JGE
+@1
+D=M
+@20
+D=D+M
+A=D
+D=M
+@30
+M=D
+@1
+D=M
+@20
+D=D+M
+@1
+D=D+A
+A=D
+D=M
+@40
+M=D
+@30
+D=M
+@40
+D=D-M
+@NOSWAP
+D;JLE
+@40
+D=M
+@1
+A=M
+@20
+A=A+M
+M=D
+@30
+D=M
+@1
+A=M
+@20
+A=A+M
+@1
+A=A+1
+M=D
+(NOSWAP)
+@20
+M=M+1
+@INNER
+0;JMP
+(NEXT)
+@10
+M=M-1
+@OUTER
+0;JMP
+(END)
+@0
+M=-1
+@END
+0;JMP
