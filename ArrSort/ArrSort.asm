778a90e2b4825c5a9205e4043ae03d9720281d0d
DuyDo24 <a1921156@adelaide.edu.au>
2025-09-10 14:27:59 +0930

    f

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -1,66 +1,103 @@
-@R2
-D=M-1
-@R3
-M=D
-(OUTER_LOOP)
-@R3
-D=M
-@END
-D;JLE
-@R4
-M=0
-(INNER_LOOP)
-@R4
-D=M
-@R3
-D=D-M
-@NEXT_PASS
-D;JGE
-@R1
-D=M
-@R4
-D=D+M
-@R5
-M=D
-A=D
-D=M
-@R6
-M=D
-@R5
-A=M+1
-D=M
-@R7
-M=D
-@R6
-D=M
-@R7
-D=D-M
-@CONTINUE_INNER_LOOP
-D;JLE
-@R7
-D=M
-@R5
-A=M
-M=D
-@R6
-D=M
-@R5
-A=M+1
-M=D
-(CONTINUE_INNER_LOOP)
-@R4
-M=M+1
-@INNER_LOOP
-0;JMP
-(NEXT_PASS)
-@R3
-M=M-1
-@OUTER_LOOP
-0;JMP
-(END)
-@R0
-M=-1
-(HALT)
-@HALT
-0;JMP
 
+ @R1 // the address of arr[0]
+    D=M-1 // substract 1 because the array starts from arr[0]
+    @R2 // the length
+    M=M+D // now R2 is the address of the last element
+
+(OUTER_LOOP)
+(CHECK_TERMINATE)
+    @R1
+    D=M
+    @R2
+    D=D-M
+    @FINISH
+    D;JGT
+    @R1
+    D=M
+    @R3 // use R3 as the index of the inner loop.
+    M=D+1 
+
+
+(INNER_LOOP)
+(CHECK_INNER_END)
+    @R3
+    D=M
+    @R2
+    D=D-M
+    @INNER_FINISH
+    D;JGT
+    @R3 // use inner index to locate the element.
+    A=M
+    D=M // now D contains the element pointed by the inner index.
+    @ELEM_POS
+    D;JGE
+    @ELEM_NEG
+    0;JMP
+(SWAP) // swap the value pointed by the inner and outer index pointer
+    @R1
+    A=M
+    D=M
+    @temp
+    M=D
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    M=D
+    @temp
+    D=M
+    @R3
+    A=M
+    M=D
+(SKIP)
+    @R3
+    M=M+1
+    @INNER_LOOP
+    0;JMP
+
+(INNER_FINISH)
+    @R1
+    M=M+1
+    @OUTER_LOOP
+    0;JMP
+
+(FINISH)
+    @R0
+    M=-1
+(END)
+    @END
+    0;JMP
+
+(REF_NEG)
+
+(REF_POS)
+    // subs
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    D=D-M // substraction, may cause Overflow!
+    @SKIP
+    D;JGE
+    @SWAP
+    0;JMP
+
+(ELEM_NEG)
+    @R1
+    A=M
+    D=M
+    @REF_NEG
+    D;JLT
+    @SWAP
+    0;JMP
+
+(ELEM_POS)
+    @R1
+    A=M
+    D=M
+    @REF_POS
+    D;JGE
+    @SKIP
+    0;JMP
